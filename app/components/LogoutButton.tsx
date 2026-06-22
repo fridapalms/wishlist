e61625c36037dfe087f31a40a5283d2abee9f868
Frida <fridapalms@live.se>
2026-06-22 12:46:54 +0200

    changed logout btn

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -1,12 +1,13 @@
-"use client";
 import { LogOut } from "lucide-react";
 import { logout } from "../actions/authActions";
 
 export const LogoutButton = () => {
   return (
-    <button onClick={() => logout()} className="flex flex-row gap-2 items-center justify-center text-[#BF5048] border p-1.5 transition delay-150 ease-in-out hover:bg-[#DFC6C0] font-semibold">
-      <LogOut />
-      <span className="hidden lg:block">LOG OUT</span>
-    </button>
+    <form action={logout}>
+      <button className="flex flex-row gap-2 items-center justify-center text-[#BF5048] border p-1.5 transition delay-150 ease-in-out hover:bg-[#DFC6C0] font-semibold">
+        <LogOut />
+        <span className="hidden lg:block">LOG OUT</span>
+      </button>
+    </form>
   );
 };
